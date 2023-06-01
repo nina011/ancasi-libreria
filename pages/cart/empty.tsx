a46175adada765a55b29b714722b7664fae487bd
nina011 <javieraburgospaez@gmail.com>
2023-05-31 22:17:31 -0400

    pagina de inicio del carrito de compras y tambien componente empty

---
+++ pages/cart/empty.tsx
@@ -0,0 +1,48 @@
+import NextLink from 'next/link'
+
+import { ShopLayout } from '../../components/layouts'
+import { Box, Link, Typography } from '@mui/material'
+import { RemoveShoppingCartOutlined } from '@mui/icons-material'
+
+const EmptyPage = () => {
+    return (
+        <ShopLayout
+            title='Carrito vacío'
+            pageDescription='No hay artículos en el carrito de compras'
+        >
+            <Box
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+                height='calc(100vh - 200px'
+                sx={{ flexDirection: { xs: 'column', sm:'row' }}}
+            >
+                <RemoveShoppingCartOutlined />
+                <Box
+                    display={'flex'}
+                    flexDirection={'column'}
+                    alignItems={'center'}
+                >
+                    <Typography
+                        margin={'left'}
+                    >
+                        Su carrito está vacío
+                    </Typography>
+                    <NextLink
+                        href='/'
+                        passHref    
+                    >
+                        <Link
+                            typography='h4'
+                            color='secondary'
+                        >
+                            Regresar
+                        </Link>
+                    </NextLink>
+                </Box>
+            </Box>
+        </ShopLayout>
+    )
+}
+
+export default EmptyPage
