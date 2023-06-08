f6dd20c36052d907a1f4f810dc5eedb674e26175
nina011 <javieraburgospaez@gmail.com>
2023-06-08 16:32:28 -0400

    ahora las imagenes tienen un loading

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -10,13 +10,14 @@ interface Props {
 export const ProductCard: FC<Props> = ({ product }) => {
 
     const [isHovered, setIsHovered] = useState(false)
+    const [isImageLoaded, setIsImageLoaded] = useState(false)
 
     const productImage = useMemo(() => {
         return isHovered
             ? `products/${product.images[1]}`
             : `products/${product.images[0]}`
     }, [isHovered, product.images])
-
+    
     return (
         <Grid
             item
@@ -43,7 +44,7 @@ export const ProductCard: FC<Props> = ({ product }) => {
                                 className='fadeIn'
                                 image={productImage}
                                 alt={product.title}
-                                onLoad={() => console.log('cargando...')}
+                                onLoad={() => setIsImageLoaded(true)}
                             />
                         </CardActionArea>
                     </Link>
@@ -51,7 +52,7 @@ export const ProductCard: FC<Props> = ({ product }) => {
             </Card>
 
             <Box
-                sx={{ mt: 1 }}
+                sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }}
                 className='fadeIn'
             >
                 <Typography
