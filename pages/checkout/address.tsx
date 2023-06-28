85b622326b01b7dfb8fbfa94bfdc41ab306e8d2a
nina011 <javieraburgospaez@gmail.com>
2023-06-27 23:40:06 -0400

    trabajando en formulario de address validando token y redireccionando cuando no haya token o no sea valido

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react'
 import { ShopLayout } from '../../components/layouts'
 import { Box, Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
+import { NextServer } from 'next/dist/server/next'
+import { GetServerSideProps } from 'next'
+import { jwt } from '../../utils'
 
 const AddressPage: FC = () => {
   return (
@@ -138,4 +141,34 @@ const AddressPage: FC = () => {
   )
 }
 
+export const getServerSideProps: GetServerSideProps = async(ctx) => {
+    
+    const { req } = ctx;
+    const { token = '' } = req.cookies;
+
+    let isValidToken = false;
+
+    try{    
+        await jwt.isValidToken( token )
+        isValidToken = true;
+
+    }catch(err){
+        isValidToken = false;
+    }
+
+    if( !isValidToken ){
+        return {
+            redirect: {
+                destination: '/auth/login?p=/checkout/address',
+                permanent: false
+            }
+        }
+    }
+
+    return{
+        props:{
+            
+        }
+    }
+}
 export default AddressPage
